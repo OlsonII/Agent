27aa72d2be7c576d168f0561340fc23f6de86a92
Nilson Rosado <30422101+OlsonII@users.noreply.github.com>
2021-01-17 11:30:05 -0500

    INITIAL COMMIT

---
+++ Agent/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Management;
+
+namespace Agent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialProvider provider = new SerialProvider();
+            Console.WriteLine(provider.GetSerial());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Agent/SerialProvider.cs
@@ -0,0 +1,28 @@
+﻿namespace Agent
+{
+    
+    using System;
+    using System.Management;
+
+    public class SerialProvider
+    { 
+        private SelectQuery Query;
+        private ManagementObjectSearcher Searcher;
+
+        public SerialProvider()
+        {
+            this.Query = new SelectQuery("SELECT * FROM Win32_ComputerSystemProduct");
+            this.Searcher = new ManagementObjectSearcher(Query);
+        }
+
+        public string GetSerial()
+        {
+            foreach (ManagementObject managementObject in Searcher.Get())
+            {
+                string identifiyingNumber = managementObject.ToString().Split(',')[0].Split('=')[1];
+                return identifiyingNumber;
+            }
+            return string.Empty;
+        }
+    }
+}
